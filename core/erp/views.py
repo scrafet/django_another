4fe5b8f927a4924ab42c33ef6b6dd153ab60f5fd
scrafet <scrafet@gmail.com>
2023-11-18 12:13:55 -0500

    se agregaron modelos, una vista y el archivo choises

--- core/erp/views.py
+++ core/erp/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from core.erp.models import Category
+
 
 # Create your views here.
 def myfirstview(request):
-    data={
-        'name' : 'William'
+    data = {
+        'name': 'William',
+        'categories': Category.objects.all()
     }
-    return render(request, 'index.html',data)
+    return render(request, 'index.html', data)
+
+
+def mysecondview(request):
+    data = {
+        'name': 'William',
+        'categories': Products.objects.all()
+    }
+    return render(request, 'index.html', data)
