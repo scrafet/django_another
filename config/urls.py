6f7617be930c9167ca2b909980d696c1b63106d1
scrafet <scrafet@gmail.com>
2023-12-26 05:08:12 -0500

    testeando datatables

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from core.erp.views import myfirstview
+# from core.erp.views import myfirstview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
---
+++ core/erp/views/category/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from core.erp.models import Category
+from django.views.generic import ListView
+
+
+def category_list(request):
+    data = {
+        'title': 'Listado de Categorias',
+        'categories': Category.objects.all(),
+    }
+    return render(request, 'category/list.html', data)
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de Categorías'
+        return context
